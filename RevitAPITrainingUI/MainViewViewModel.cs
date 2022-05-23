fe7bed9c44fe90102d1b64876b259c35395a972c
Татьяна Буянова <tanyasilagina@gmail.com>
2022-05-24 01:42:24 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingUI/MainViewViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using RevitAPITrainingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingUI
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public DelegateCommand SaveCommand { get; }
+        public List<Element> PickedObjects { get; } = new List<Element>();
+        public List<WallType> WallSystems { get; } = new List<WallType>();
+
+        public WallType SelectedWallType { get; set; }
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            SaveCommand = new DelegateCommand(OnSaveCommand);
+            PickedObjects = SelectionUtils.PickObjects(commandData);
+            WallSystems = WallsUtils.GetWallType(commandData);
+
+        }
+
+        
+
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            if (PickedObjects.Count == 0 || SelectedWallType == null)
+                return;
+
+            using (var ts = new Transaction(doc, "Set system type"))
+            {
+                ts.Start();
+
+                foreach (var pickedObject in PickedObjects)
+                {
+                    if (pickedObject is Wall)
+                    {
+                        var oWall = pickedObject as Wall;
+                        oWall.ChangeTypeId(SelectedWallType.Id);
+                    }
+                }
+
+                ts.Commit();
+            }
+
+            RaiseCloseRequest();
+        }
+
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
